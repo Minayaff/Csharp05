afd4fee20dcc57767af0677c4f5c1ac93b1d5883
Mina <41920626+Minayaff@users.noreply.github.com>
2022-05-18 23:04:13 +0400

    Add files via upload

---
+++ Lesson-1/Csharp05Group/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace Csharp05Group
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string test = "salam kainat";
+            Console.WriteLine();
+            //Console.WriteLine(test);
+            //Console.WriteLine(test);
+            //Console.WriteLine(test);
+            //Console.WriteLine(test);
+            //Console.WriteLine(test);
+            //Console.WriteLine(test);
+            //Console.WriteLine(test);
+            //Console.WriteLine(test);
+
+            //bu kod adi ekrana cixartmaq ucun lazim olur
+            //string name = "Mənim adım Minadır";
+            //Console.WriteLine(name);
+            //Console.ReadKey();
+
+            //byte num = 255;
+            
+            //int num2 =  Convert.ToInt32("5cctdxd");
+            //long num3 = 5;
+
+            //float f1 = 5F / 3;
+            //double db1 = 5D / 3;
+            //decimal dec1 = 5M / 3;
+
+            //Console.WriteLine(num2);
+
+            //Console.WriteLine(f1 +" float");
+            //Console.WriteLine(db1 +" double");
+            //Console.WriteLine(dec1 + " decimal");
+
+            ////char m = 'M';
+
+            ////bool yes = true;
+
+            string five = "5";
+            Console.WriteLine(Type.GetType(five));
+        }
+
+    }
+}
---
+++ Lesson-1/test.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace TestCsharp
+{
+   class Program
+{
+     static void Main(string[] args)
+    {
+        Console.WriteLine("Please write your name");
+       string word=Console.ReadLine();
+       Console.WriteLine(word +" Hello");
+       Console.ReadKey();
+
+    }
+
+   }
+}
