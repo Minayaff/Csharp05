f4df7a5ae08c4c948bf50fff90bda8dca2ef12b5
Mina <41920626+Minayaff@users.noreply.github.com>
2022-05-27 16:44:42 +0400

    Add files via upload

---
+++ Lesson-3/Lesson2Csharp05/Lesson2Csharp05/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Lesson2Csharp05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Hello_World();
+            //Console.WriteLine(Topla());
+            //long c = Topla() + 100;
+            //Console.WriteLine(c);
+            //Topla2();
+            //int x = Topla2() + 1000;
+
+            Console.WriteLine(Topla3(5));
+            HiPerson("Nofel");
+            HiPerson("Mina");
+
+        }
+        public static void Topla2()
+        {
+            int x = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                x += i;
+            }
+            Console.WriteLine(x);
+        }
+
+        public static int Topla()
+        {
+            byte x = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                //x += i;
+            }
+           
+            return x;
+        }
+
+
+        public static int Topla3(int number)
+        {
+            int x = 0;
+            for (int i = 1; i < number; i++)
+            {
+                x += i;
+            }
+
+            return x;
+        }
+
+        public static void HiPerson(string name)
+        {
+            Console.WriteLine($"Salamm {name}");
+
+        }
+
+    }
+}
---
+++ Lesson-3/Lesson2Csharp05/Lesson2Csharp05/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson2Csharp05
+{
+    internal class Student
+    {
+    }
+}
